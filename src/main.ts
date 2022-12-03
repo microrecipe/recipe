f7f5bd32efabb186dea6617dcffceb26047eb6fa
Rhazes Avicenna <rhazes@binaridigital.tech>
2022-12-03 16:31:18 +0700

    fix: start grpc microservice

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { Logger } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { NestFactory } from '@nestjs/core';
-import { KafkaOptions, Transport } from '@nestjs/microservices';
+import { GrpcOptions, KafkaOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -24,7 +25,18 @@ async function bootstrap() {
     },
   });
 
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'recipes',
+      protoPath: join(__dirname, '../src/proto/recipes.proto'),
+      url: `${process.env.RECIPE_HOST}:${process.env.RECIPE_GRPC_PORT}`,
+    },
+  });
+
   await app.startAllMicroservices();
+
+  logger.log(`gRPC service running on port: ${process.env.RECIPE_GRPC_PORT}`);
 
   await app.listen(process.env.RECIPE_REST_PORT);
 
