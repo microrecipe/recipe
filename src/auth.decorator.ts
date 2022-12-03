3264fe04f6be4d0712138def8379c1c637cd1a10
Rhazes Avicenna <rhazes@binaridigital.tech>
2022-12-03 16:30:04 +0700

    refactor: file structure

--- src/auth.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { TokenPayload } from './recipes.interface';
-
-export const UserPayload = createParamDecorator(
-  (data: any, context: ExecutionContext): TokenPayload => {
-    const req = context.switchToHttp().getRequest();
-    return req.user;
-  },
-);
